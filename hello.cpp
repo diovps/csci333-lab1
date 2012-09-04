498b69e1c6f50da155edd0a72c9e487efbb57866
diovps <nrharris@unca.edu>
2012-09-04 12:25:53 -0400

    Changed from hello world to project Euler

--- hello.cpp
+++ hello.cpp
@@ -3,9 +3,21 @@
 using std::cout;
 using std::endl;
 
+int sums(int);
+
 int main(){
 
-	cout << "Hello World" << endl;
+	cout << sums(1000) << endl;
 	return 0;
-
 }
+
+int sums(int N){
+	int sums = 0;
+	for(int i = 0; i < N; i++){
+		if(i%3==0 || i%5==0){
+			sums+=i;
+		}
+	}
+	return sums;
+}
+
